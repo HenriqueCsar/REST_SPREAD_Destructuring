dacd33dfe56d39a392c41a9782b8b188d30f746d
HenriqueCsar <60591111+HenriqueCsar@users.noreply.github.com>
2021-06-10 22:13:25 -0300

    Update rest.js

--- rest.js
+++ rest.js
@@ -13,10 +13,18 @@ console.log(sum(1,2,3,4,5,6,7));
 //Using the new method ES6
 
 function sum_rest(...args){
-    //typeof(args) == 'array';
-    //array.reduce((accm, currentV) => accm + currentV);
    return args.reduce((accumulator, currentValue) => accumulator + currentValue); //using reduce...
 } //Rest Operator
 
 
 console.log(sum_rest(1,2,3,4,5,6,7))
+
+
+
+//with two variables
+
+function sum_rest_two_varibles(a, b, ...rest){
+    return rest.reduce((acc, curr) => a+b+acc+curr)
+}
+
+console.log(sum_rest_two_varibles(1,3,4,5,6,7))
